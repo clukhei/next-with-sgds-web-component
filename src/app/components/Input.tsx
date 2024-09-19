3dca495c8817842fcc7264170a8bb2bd491dff3c
Lu Khei Chong <kheiboardwarrior@gmail.com>
2024-09-19 16:43:29 +0800

    add events handling and attribute handling in input example

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,8 +1,21 @@
-import SgdsInput from "@govtechsg/sgds-web-component/react/input/index.js";
+'use client'
+import SgdsReactInput from "@govtechsg/sgds-web-component/react/input/index.js";
+import { useState } from 'react';
+import type { SgdsInput as SgdsInputElement } from '@govtechsg/sgds-web-component/components';
+
 
 export const Input = () => {
+  const [value, setValue] = useState('');
+  const handleInputChange = (event: Event) => {
+    const target = event.target as SgdsInputElement
+    setValue(target.value)
+  }
+
   return (
-    <SgdsInput
+    <>
+    <SgdsReactInput
+      disabled={ value === 'disabled'}
+      onSgdsInput={handleInputChange}
       type="text"
       label="Label"
       hintText="This is a hint text"
@@ -13,6 +26,8 @@ export const Input = () => {
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                     </svg>'
       placeholder="Placeholder"
-    ></SgdsInput>
+    ></SgdsReactInput>
+    <h1>value: {value}</h1>
+    </>
   );
 };
